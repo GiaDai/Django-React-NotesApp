65dadb7ddae3ee11e230e000fefe8b45144e1dc2
Le Anh Toan <leanhtoan@vietbank.com.vn>
2024-02-16 11:05:36 +0700

    Initial project

---
+++ mynotes/api/views.py
@@ -0,0 +1,60 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import NoteSerializer
+from .models import Note
+# Create your views here.
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        {
+            'Endpoint': '/notes/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns an array of notes'
+        },
+        {
+            'Endpoint': '/notes/id',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a single note object'
+        },
+        {
+            'Endpoint': '/notes/create/',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Creates new note with data sent in post request'
+        },
+        {
+            'Endpoint': '/notes/id/update/',
+            'method': 'PUT',
+            'body': {'body': ""},
+            'description': 'Creates an existing note with data sent in post request'
+        },
+        {
+            'Endpoint': '/notes/id/delete/',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Deletes and exiting note'
+        },
+    ]
+    # explain the safe=False
+    # The safe parameter in the JsonResponse() function is used to specify whether or not the data is safe for use in the template. If the safe parameter is set to False, the data is not safe for use in the template. If the safe parameter is set to True, the data is safe for use in the template.
+    return Response(routes)
+
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getNote(request, pk):
+    note = Note.objects.get(id=pk)
+    serializer = NoteSerializer(note, many=False)
+    # check the serializer.data is null return 404 not found
+
+    if serializer.data is None:
+        return Response({'detail': 'Note does not exist'})
+    
+    return Response(serializer.data)
